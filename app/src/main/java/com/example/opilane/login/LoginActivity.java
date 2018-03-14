2b25be4ee9c839c8ebe060817ddbd73db7444471
opilane <helena.tarum@hotmail.com>
2018-03-14 13:34:44 +0200

    1

--- app/src/main/java/com/example/opilane/login/LoginActivity.java
+++ app/src/main/java/com/example/opilane/login/LoginActivity.java
@@ -1,13 +1,23 @@
 package com.example.opilane.login;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.google.firebase.auth.FirebaseAuth;
+import android.widget.Toast;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
 
     int loendaja = 3;
     private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,74 @@ public class LoginActivity extends AppCompatActivity {
         registreeri = findViewById(R.id.registreeri);
         unustatud = findViewById(R.id.unustatud);
         katse = findViewById(R.id.katse);
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
 
+        FirebaseUser kasutaja = firebaseAuth.getCurrentUser();
+        if (kasutaja != null){
+            finish();
+            startActivity(new Intent(LoginActivity.this, UserProfileActivity.class));
+        }
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valideeri (epost.getText().toString(), salasõna.getText().toString());
+            }
+        });
+        registreeri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registreerimine = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registreerimine);
+
+            }
+        });
+        unustatud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent unustatud_sala = new Intent(LoginActivity.this, PasswordActivity.class);
+                startActivity(unustatud_sala);
+            }
+        });
+
+    }
+    private void valideeri (String epost, String salasõna){
+        progressDialog.setMessage("Andmete edastamisega läheb aega, palun kannatust!");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(epost, salasõna).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    progressDialog.dismiss();
+                    kontrolliEposti();
+                }
+                else {
+                    teade("Sisselogimine ebaõnnestus!");
+                    if (loendaja < 3){
+                        katse.setText("Katseid on jäänud veel" + String.valueOf(loendaja));
+                    }
+                    if (loendaja == 0){
+                        btn_login.setEnabled(false);
+                        katse.setText("Võta ühendust administraatoriga");
+                    }
+                }
+            }
+        });
+
+    }
+    private void kontrolliEposti(){
+        FirebaseUser firebaseUser = firebaseAuth.getInstance().getCurrentUser();
+        boolean epostiKontroll = firebaseUser.isEmailVerified();
+        if (epostiKontroll){
+            finish();
+            startActivity(new Intent(LoginActivity.this, UserProfileActivity.class));
+        }
+        else {
+            teade ("Kinnitage oma eposti aadress!");
+        }
+    }
+    public void teade(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
